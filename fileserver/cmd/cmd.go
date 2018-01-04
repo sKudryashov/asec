831362f7c3fa8a0ce93f982d970e9a58340a7ba5
Sergey Kudryashov <iden.82@gmail.com>
2018-01-04 16:15:57 +0200

    Cobra experience finished up

--- fileserver/cmd/cmd.go
+++ fileserver/cmd/cmd.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/sKudryashov/asec/fileserver/servd"
 	"github.com/spf13/cobra"
 )
@@ -9,19 +11,13 @@ var autoTLS bool
 var port int
 var help bool
 
-// func init() {
-// 	autoTLS = flag.Bool("autotls", false, "Launch server with auto TLS *required")
-// 	port = flag.Int("port", 80, "Launch server with given port")
-// 	help = flag.Bool("help", false, "Print cmd help")
-
-// 	flag.Parse()
-// }
-
 func main() {
 	rootCmd := &cobra.Command{
-		Use:   "filesrv",
-		Short: "start fileserver",
-		Run:   cmdHandler,
+		Use:                "filesrv",
+		Short:              "start fileserver",
+		Run:                cmdHandler,
+		Args:               argsHandler,
+		DisableFlagParsing: true,
 	}
 	rootCmd.PersistentFlags().BoolVar(&autoTLS, "autotls", false, "Launch server with auto TLS")
 	rootCmd.PersistentFlags().IntVar(&port, "port", 80, "Launch server with given port")
@@ -34,4 +30,11 @@ func cmdHandler(cmd *cobra.Command, args []string) {
 	} else {
 		servd.StartServer(port, autoTLS)
 	}
+}
+
+func argsHandler(cmd *cobra.Command, args []string) error {
+	if len(args) < 3 {
+		return fmt.Errorf("too few args")
+	}
+	return nil
 }
